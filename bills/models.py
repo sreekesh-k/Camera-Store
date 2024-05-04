406f6eec5d3986c5682aa2f1b32afed0f77a2f05
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-04 17:02:49 +0530

    model and migrations for bills

--- bills/models.py
+++ bills/models.py
@@ -1,3 +1,15 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+from sales.models import SalesCamera 
+
+class SalesBilling(models.Model):
+    camera = models.ForeignKey(SalesCamera, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    sales_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sales_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Sales Bill - {self.id}"
--- rentalBills/models.py
+++ rentalBills/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from rental.models import RentCamera 
+from django.utils import timezone
 
-# Create your models here.
+class RentalBilling(models.Model):
+    camera = models.ForeignKey(RentCamera, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Sales Bill - {self.id}"

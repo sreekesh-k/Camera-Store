a8d1327b2b2096ec7167e0fd649a0cb66bd45adc
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-04 16:14:24 +0530

    Created apps

--- cameraStore/settings.py
+++ cameraStore/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'customAdmin'
+    'customAdmin',
+    'sales',
+    'rental',
+    'bills'
 ]
 
 MIDDLEWARE = [

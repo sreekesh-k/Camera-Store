5bc3a73c75a3c63bf3d79e31da65cf55461c06f3
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-06 14:46:15 +0530

    view and add stock

--- sales/urls.py
+++ sales/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('viewSales/', views.viewSales,name="viewSales"),
     path('addSales/', views.addSales,name="addSales"),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from . models import SalesCamera 
 # Create your views here.
-def addSales(request):
+def viewSales(request):
     pagehead = "Sales"
     object = SalesCamera.objects.all()
     return render(request,"viewSales.html",{"stocks":object,"pagehead":pagehead})
 
 
+def addSales(request):
+    pagehead = "Add Sales"
+    return render(request,"addSales.html",{"pagehead":pagehead})

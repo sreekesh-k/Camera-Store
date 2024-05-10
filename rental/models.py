a94467f2ec7d1942c68fd7840f45f48b5703ddfb
Paul-sharon <paulsharons66@gmail.com>
2024-05-10 19:08:27 +0530

    by pss

--- rental/models.py
+++ rental/models.py
@@ -7,6 +7,7 @@ class RentCamera(models.Model):
     model_number = models.CharField(max_length=100)
     specifications = models.TextField()
     photo = models.ImageField(upload_to='images/')
+    isAvailable =models.BooleanField(default=True)
     charge_per_day = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
--- sales/models.py
+++ sales/models.py
@@ -14,6 +14,7 @@ class SalesCamera(models.Model):
     specifications = models.TextField()
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     photo = models.ImageField(upload_to='images/')
+    isAvailable =models.BooleanField(default=True)
     MRP = models.DecimalField(max_digits=10, decimal_places=2)
     purchased_date = models.DateField()
     type = models.CharField(max_length=50, choices=CAMERA_TYPES)

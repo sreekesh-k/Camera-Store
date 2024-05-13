0640e2a509f85422d76a920f3a0a16dabdbbd5f3
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-13 22:42:17 +0530

    f

--- bills/views.py
+++ bills/views.py
@@ -3,8 +3,15 @@ from sales.models import SalesCamera
 from . models import SalesBilling
 # Create your views here.
 def customer(request,pk):
-    # if request.method == 'POST':
-
-    itemToDisplay = SalesCamera.objects.get(pk=pk)
+    if request.method == 'POST':
+        customer_name = request.POST.get('customer_name')
+        customer_phno = request.POST.get('customer_phno')
+        customer_address =request.POST.get('customer_address')
+        customer_email =request.POST.get('customer_email')
+        new_bill = SalesBilling(camera=pk,customer_name=customer_name,customer_phno=customer_phno,
+                                            customer_address=customer_address,customer_email=customer_email
+                                            )
+    
+    
     pagehead = "CustomerDetails"
     return render(request,"CustomerDetails.html",{"stock":itemToDisplay,"pagehead":pagehead})

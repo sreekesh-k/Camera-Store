26e9c06a725e2f308fb543a67f75f5d944fc5c0c
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-24 17:46:29 +0530

    build 1

--- cameraStore/settings.py
+++ cameraStore/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'cameraStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'XoAANFonXbaBbInWHgVaZGQUeuQDKNGM',
+        'HOST':'monorail.proxy.rlwy.net',
+        'PORT':'59122'
     }
 }
 
